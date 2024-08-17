8f99f2085dba521c166ffed96b05f4d64a6a8353
tunardev <tunardev@gmail.com>
2024-08-17 12:40:33 +0400

    feat: implement Calculator class with add, subtract, multiply, and di…

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -12,25 +12,25 @@ def test_add():
     assert result == 80
 
 
-def test_multiply():
-    operands = operands_factory(45, 35)
-    result = calculator.multiply(operands)
-    assert result == 1575
-
-
 def test_subtract():
     operands = operands_factory(45, 35)
     result = calculator.subtract(operands)
     assert result == 10
 
 
-def test_divide():
-    operands = operands_factory(45, 5)
-    result = calculator.divide(operands)
-    assert result == 9
+def test_multiply():
+    operands = operands_factory(45, 35)
+    result = calculator.multiply(operands)
+    assert result == 1575
 
 
 def test_divide_by_zero():
     operands = operands_factory(45, 0)
     with pytest.raises(ValueError, match="Cannot divide by zero"):
         calculator.divide(operands)
+
+
+def test_divide():
+    operands = operands_factory(45, 5)
+    result = calculator.divide(operands)
+    assert result == 9
