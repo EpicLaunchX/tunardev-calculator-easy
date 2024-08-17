58c221208136210c5018bc80488fd8ccae4be7f1
tunardev <tunardev@gmail.com>
2024-08-17 12:02:47 +0400

    feat: add Operands

--- tests/test_operands.py
+++ tests/test_operands.py
@@ -8,20 +8,24 @@ def test_operands_initialization():
     assert operands.first_operand == 5
     assert operands.second_operand == 10
 
+
 def test_operands_type():
     operands = Operands(first_operand=5, second_operand=10)
     assert isinstance(operands.first_operand, int)
     assert isinstance(operands.second_operand, int)
+
 
 def test_operands_negative_values():
     operands = Operands(first_operand=-5, second_operand=-10)
     assert operands.first_operand == -5
     assert operands.second_operand == -10
 
+
 def test_operands_zero_values():
     operands = Operands(first_operand=0, second_operand=0)
     assert operands.first_operand == 0
     assert operands.second_operand == 0
+
 
 def test_operands_large_values():
     operands = Operands(first_operand=1_000_000, second_operand=2_000_000)
